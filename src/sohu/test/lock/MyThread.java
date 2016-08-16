0ebfe2076f22592655c4e168ccd0a8c0c29d3480
Gao Qi(北京研发中心) <qigao212074@sohu-inc.com>
2016-08-16 14:16:03 +0800

    some annotation in lock

--- src/sohu/test/lock/MyThread.java
+++ src/sohu/test/lock/MyThread.java
@@ -1,6 +1,9 @@
 package sohu.test.lock;
 
 /**
+ * 1.syn 当一个线程获取了资源后，其他线程必须等待，但是lock  其他的线程可以Interrupt停止等待。
+ * 2.lock最后一定要unlock()释放资源，不然资源一直被占用
+ * 3.lock可以获取线程是否获取锁的状态，tryLock()
  * Created by qigao212074 on 2016/8/15.
  */
 class MyThread extends Thread {
